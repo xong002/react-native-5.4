ddea75c33c032bb19d4a19a9a0ff356fd9d6d134
xong002 <115723006+xong002@users.noreply.github.com>
2023-05-28 10:02:33 +0800

    assignment 5.4

--- App.js
+++ App.js
@@ -1,13 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Button, Platform } from 'react-native';
+import * as Calendar from 'expo-calendar';
+
+
 
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      const { status } = await Calendar.requestCalendarPermissionsAsync();
+      if (status === 'granted') {
+        const calendars = await Calendar.getCalendarsAsync(Calendar.EntityTypes.EVENT);
+        console.log('Here are all your calendars:');
+        console.log({ calendars });
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Calendar Module Example</Text>
+      <Button title="Create a new calendar" onPress={createCalendar} />
+      <Button title="Test" onPress={addReminder} />
     </View>
   );
+}
+
+// const [calendarID, setCalendarID] = useState(null);
+
+async function addReminder() {
+// const reminder = {
+//   startDate: '2023-05-19',
+//   completionDate: '2023-05-19',
+//   title: 'Test Reminder'
+// }
+//   const newReminder = Calendar.createReminderAsync({calendarID}, reminder)
+}
+
+async function getDefaultCalendarSource() {
+  const defaultCalendar = await Calendar.getDefaultCalendarAsync();
+  return defaultCalendar.source;
+}
+
+async function createCalendar() {
+  const defaultCalendarSource =
+    Platform.OS === 'ios'
+      ? await getDefaultCalendarSource()
+      : { isLocalAccount: true, name: 'Expo Calendar' };
+  const newCalendarID = await Calendar.createCalendarAsync({
+    title: 'Expo Calendar',
+    color: 'blue',
+    entityType: Calendar.EntityTypes.EVENT,
+    sourceId: defaultCalendarSource.id,
+    source: defaultCalendarSource,
+    name: 'internalCalendarName',
+    ownerAccount: 'personal',
+    accessLevel: Calendar.CalendarAccessLevel.OWNER,
+  });
+  console.log(`Your new calendar ID is: ${newCalendarID}`);
+  // setCalendarID(newCalendarID);
 }
 
 const styles = StyleSheet.create({
